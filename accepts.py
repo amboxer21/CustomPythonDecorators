11d046476f9a037267c550a5136b85fd8bfbad7f
Anthony Guevara <amboxer21@gmail.com>
2018-09-18 10:46:30 -0400

    Update accepts.py

--- accepts.py
+++ accepts.py
@@ -5,16 +5,18 @@ class Accepts(object):
     @staticmethod
     def boolean(func):
         def wrapper(*args):
-            for arg in args:
-                if not str(arg) in ('True','False'):
-                    raise TypeError('"' + str(arg) + '" is not a bool type!')
+            length = len(args)
+            for index in range(1,(int(length))):
+                if not str(args[int(index)]) in ('True','False'):
+                    raise TypeError('"' + str(args[int(index)]) + '" is not a bool type!')
             return func
         return wrapper
     @staticmethod
     def integer(func):
         def wrapper(*args):
-            for arg in args:
-                if re.search(r'\d+\b', arg) is None:
-                    raise TypeError('"' + str(arg) + '" is not an integer!')
+            length = len(args)
+            for index in range(1,(int(length))):
+                if re.search(r'\d+\b', args[int(index)]) is None:
+                    raise TypeError('"' + str(args[int(index)]) + '" is not an integer!')
             return func
         return wrapper
