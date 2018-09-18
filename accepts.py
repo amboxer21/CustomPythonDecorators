b49de7ea65e31af76bd27e9793cd0fcd8cbcaa4f
Anthony Guevara <amboxer21@gmail.com>
2018-09-18 14:40:49 -0400

    Update accepts.py

--- accepts.py
+++ accepts.py
@@ -1,22 +1,23 @@
-#!/usr/bin/env python
-
-# Call decorators with @Accepts.boolean, @Accepts.integer, etc. There are examples in the readme!
 class Accepts(object):
+    
     @staticmethod
     def boolean(func):
         def wrapper(*args):
-            length = len(args)
-            for index in range(1,(int(length))):
-                if not str(args[int(index)]) in ('True','False'):
-                    raise TypeError('"' + str(args[int(index)]) + '" is not a bool type!')
+            for arg in args:
+                if re.search(r'<__main__',str(arg)) is not None:
+                    pass
+                elif not str(arg) in ('True','False'):
+                    raise TypeError('"' + str(arg) + '" is not a bool type!')
             return func
         return wrapper
+    
     @staticmethod
     def integer(func):
         def wrapper(*args):
-            length = len(args)
-            for index in range(1,(int(length))):
-                if re.search(r'\d+\b', args[int(index)]) is None:
-                    raise TypeError('"' + str(args[int(index)]) + '" is not an integer!')
+            for arg in args:
+                if re.search(r'<__main__',str(arg)) is not None:
+                    pass
+                elif re.search(r'\d+\b', str(arg)) is None:
+                    raise TypeError('"' + str(arg) + '" is not an integer!')
             return func
         return wrapper
