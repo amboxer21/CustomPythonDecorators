f9d56eda1dd3559f22bc71421eb7f57434ad1753
amboxer21 <amboxer21@gmail.com>
2018-09-19 13:05:49 -0400

    Adding setup.py script to modulize it.

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='accepts',
+version='0.0.1',
+url='https://github.com/amboxer21/CustomPythonDecorators',
+license='GPL-3.0',
+author='Anthony Guevara',
+author_email='amboxer21@gmail.com',
+description='This program allows you to restrict types passed to a method.',
+packages=find_packages(exclude=['tests']),
+long_description=open('README.md').read(),
+classifiers=[
+    'Intended Audience :: Developers',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: System Administrators',
+    'Development Status :: 4 - Beta',
+    'Natural Language :: English',
+    'Environment :: Console',
+    'Environment :: No Input/Output (Daemon)',
+    'Programming Language :: Python :: 2.7',
+    'Operating System :: POSIX :: Linux',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+],
+zip_safe=True,
+setup_requires=['pytailf', 'python-crontab'],)
