3b5cf4fff8eea0334691d50d3c240d94aa618407
amboxer21 <amboxer21@gmail.com>
2019-04-03 15:44:51 -0400

    Added encrypt and decrypt decorators.

---
+++ src/string.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+import re
+
+from Crypto import Random
+from Crypto.Cipher import AES
+
+class string(object):
+
+    encrypted_text = '\0'
+    __KEY__  = Random.new().read(32)
+    __IV__   = Random.new().read(16)
+
+    @classmethod
+    def encrypt(cls,func):
+        def wrapper(*arguments):
+            if not re.match('<__main__.*object at.*>',str(arguments[0])):
+                raise SyntaxError('Method must be an instance method of a class!')
+            for string in arguments[1:]:
+                key = AES.new(
+                    cls.__KEY__,
+                    AES.MODE_CFB,
+                    cls.__IV__
+                )
+                cls.encrypted_text = key.encrypt(string)
+            return func(arguments[0].__class__,cls.encrypted_text)
+        return wrapper
+
+    @classmethod
+    def decrypt(cls,func):
+        def wrapper(*arguments):
+            if not re.match('<__main__.*object at.*>',str(arguments[0])):
+                raise SyntaxError('Method must be an instance method of a class!')
+            for string in arguments[1:]:
+                key = AES.new(
+                    cls.__KEY__,
+                    AES.MODE_CFB,
+                    cls.__IV__
+                )
+                cls.encrypted_text = key.decrypt(string)
+            return func(arguments[0].__class__,cls.encrypted_text)
+        return wrapper
